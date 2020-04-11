78180e5482bcae5980fcf2fe6df4b5002163d503
David <d@vidc.dk>
2020-04-10 22:41:52 -0400

    Fixing JS to just toggle class

--- js.js
+++ js.js
@@ -1,12 +1,12 @@
 /* variables: */
-let edit = document.querySelector(".profile__edit");
+const edit = document.querySelector(".profile__edit");
 let modal = document.querySelector(".popup");
-let popClose = document.querySelector(".popup__close");
+const popClose = document.querySelector(".popup__close");
 let profileName = document.querySelector(".profile__name");
 let profileTitle = document.querySelector(".profile__title");
-let popupTitle = document.querySelector("#popup__title");
-let popupName = document.querySelector("#popup__name");
-let savebutton = document.querySelector(".popup__save");
+let popupTitle = document.querySelector(".popup__title");
+let popupName = document.querySelector(".popup__name");
+const savebutton = document.querySelector(".popup__save");
 
 /* Eventlisteners */
 edit.addEventListener("click", popupEdit);
@@ -23,22 +23,15 @@ function saveText() {
 }
 
 function popupEdit() {
-  /* popupERR1 - for some reason neither conditions are true */
-  console.log(modal.style.display);
-  if (modal.style.display == "block") {
-    modal.style.display = "none";
 
-    return;
-  } else {
-    modal.style.display = "block";
+  if (modal.classList.contains("popup_invisible")) {
+    modal.classList.toggle("popup_invisible");
     popupTitle.value = profileTitle.innerText;
     popupName.value = profileName.innerText;
     return;
-    /*
-    TODO:  make test of none specific and add an else clause with error code : popupERR1
-    When I test specifically for none it works once and then breaks.
-    it seems to be because style is applied inline- but
-    ...this sucks.. maybe it should be done with classes instead
-    but BEM doesnt like "is_visible" classes.addEventListener */
+  } else {
+    modal.classList.toggle("popup_invisible");
+
+    return;
   }
 }

17ccb5ef79e2401185459886b7a6c57fe731136f
David <d@vidc.dk>
2020-06-01 11:42:32 -0400

    Setting up the Card Class

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,5 +1,13 @@
 class Card {
-  constructor(text, link) {
-
+  constructor(text, link, template) {
+    //link and text are private
+    this._text = text;
+    this._link = link;
+    this._template = template; //this is the HTML template
   }
+  _getTemplate() {
+    // TODO : perform all the necessary actions for getting the markup here
+  }
+
+
 }

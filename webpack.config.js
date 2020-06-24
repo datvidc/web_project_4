2781c8a82aab7dac98983c48983a18d4f8d3a090
David Christoffersen <d@vidc.dk>
2020-06-24 15:15:58 -0400

    MiniCss

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
 module.export = {
   entry: {
     main: "./src/scripts/index.js"
@@ -8,6 +8,21 @@ module.export = {
     //  below takes current directory and makes it the root of the project.
     path: path.resolve(__dirname, 'public'),
     filename: "main.js",
+  }
+  module: {
+    rules: [{
+      test: /\.css$/,
+      loaders: [{
+          loader: "css-loader",
+          options: {
+            importloaders: 1
+          }
+        }
+
+        "postcss-loader"
+
+      ]
+    }]
   }
 
 }

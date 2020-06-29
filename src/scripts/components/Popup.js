ec482e9c27834bf9a7934bb10bc63f9dccea9263
David Christoffersen <d@vidc.dk>
2020-06-29 16:05:26 -0400

    popup added setEvent listeners

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -6,32 +6,34 @@ class Popup {
 
   setEventListeners() {
     /* adds a click event listener to the close icon of the popup */
+    this._popupElement.addEventListener("click", (evt) => {
+      this.close();
+      evt.stopPropagation();
+    });
   }
 
-  _handleEscClose() {
+  _handleEscClose(evt) {
     /* logic for closing the popup by pressing the Esc key. */
+    if (evt.key === "Escape") {
+      this._popupElement.classList.remove("popup_visible");
+      evt.stopPropagation();
+    }
     this.close();
   }
 
   open() {
-    this._popupElement.classList.add("popup_is-open");
+    this._popupElement.classList.add("popup_visible");
     //add event listener for Esc
-    document.addEventListener("keyup", this._handleEscClose);
+    document.addEventListener("keyup", this._handleEscClose(evt));
   }
 
   close() {
-    this._popupElement.classList.remove("popup_is-open");
+    this._popupElement.classList.remove("popup_visible");
     //add event listener for Esc
-    document.removeEventListener("keyup", this._handleEscClose);
+    document.removeEventListener("keyup", this._handleEscClose(evt));
   }
 
 }
-
-
-
-
-/*PopupWithForm extens class popup */
-
 
 
 export default Popup;

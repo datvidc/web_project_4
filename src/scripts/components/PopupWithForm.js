597a34e75e266c662c769a7819fca8c9b78efd17
David Christoffersen <d@vidc.dk>
2020-07-05 00:23:37 -0400

    no form or gravitas

--- src/scripts/components/PopupWithForm.js
+++ src/scripts/components/PopupWithForm.js
@@ -3,19 +3,24 @@ import Popup from "./Popup.js";
 
 
 export default class PopupWithForm extends Popup {
-  constructor(popupSelector, formSubmission) {
+  constructor(popupSelector, callbackFunc) {
     super(popupSelector);
-    this._formSubmission = formSubmission;
+    this._callbackFunc = callbackFunc;
     this._form = this._popupElement.querySelector('.popup__edit-form');
+    this._name = this._form.querySelector('.popup__name');
+    this._title = this._form.querySelector('.popup__title');
   }
 
   getInputValues() {
-
+    return {
+      titleValue: this._title.value,
+      nameValue: this._name.value
+    }
   }
 
   setEventListeners() {
     // first set eventListener on close button
-    this._popupElement.addEventListener("click", (evt) => {
+    this._form.addEventListener("click", (evt) => {
       this.close();
       evt.stopPropagation();
     });
@@ -27,7 +32,8 @@ export default class PopupWithForm extends Popup {
   //it modifies the close() parent method in order to reset the form once the popup is closed.
 
   close() {
-    super.close();
+
     this._form.reset();
+    super.close();
   }
 }

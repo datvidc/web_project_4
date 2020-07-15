f20153d3ca95e0b8e40654816ffb4d7539ab6d18
David Christoffersen <d@vidc.dk>
2020-07-14 21:46:54 -0400

    first build

--- src/scripts/components/UserInfo.js
+++ src/scripts/components/UserInfo.js
@@ -3,7 +3,7 @@ export default class UserInfo {
     this._userName = document.querySelector(userNameSelector);
     this._userTitel = document.querySelector(userTitelSelector);
     this._userImage = document.querySelector(userImageSelector);
-    this._userId = api.getUser();
+    this._userId = api._getUser();
   }
 
   getUserInfo() {
@@ -23,4 +23,21 @@ export default class UserInfo {
   setUserImg(imageURL) {
     this._userImage.src = imageURL;
   }
+
+
+  _getUser() {
+    fetch("https://around.nomoreparties.co/v1/group-1/users/me", {
+        headers: {
+          authorization: "3aa990c2-b590-4bfb-9403-af52e9b89792"
+        }
+      })
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+      })
+    return
+
+  }
+
+
 }
